fb73105816bd45b04bf42ecefcbe76317572fc71
solid_drink <jevonstudying187@gmail.com>
2024-05-05 20:14:38 +0700

    authentication and raw build of admin page

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- Navbar -->
+
+        <Navbar />
+
+        <!-- Navbar End -->
+
+        <!-- Sidebar -->
+
+        <Sidebar />
+
+        <!-- Sidebar End -->
+
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import Navbar from "./Navbar.vue";
+import Sidebar from "./Sidebar.vue";
+
+// Initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+</script>
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+//  User Routes
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// End
+
+// Admin Route
+
+Route::group(['prefix' => 'admin', 'middleware' => 'redirectAdmin'], function () {
+    Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    Route::post('logout', [AdminAuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+// End
+
+require __DIR__ . '/auth.php';
